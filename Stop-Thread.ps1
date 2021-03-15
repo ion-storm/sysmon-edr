a1221e851cfb34c6725476d983c8595c7eb48961
ionstorm <ionstorm@peertopeerecash.com>
2021-03-15 00:36:42 -0400

    Add Stop-Thread and Auto File Restore capability

---
+++ Stop-Thread.ps1
@@ -0,0 +1,223 @@
+﻿Function Stop-Thread {
+    <#
+        .SYNOPSIS
+            This function is used to terminate a thread on a given process.
+
+        .DESCRIPTION
+            This function is used to terminate a thread on a given process.
+
+            IMPORTANT!!!
+            Terminating threads can potentially lead to bigger issues with a process
+            such as corrupting the process or introducing memory leaks. Make sure that
+            you understand what will happen when terminating a thread that you did not
+            build.
+
+        .PARAMETER ThreadID
+            The thread ID on a given process to terminate.
+
+        .NOTES
+            Name: Stop-Thread
+            Author: Boe Prox
+            Version History:
+                1.0 //Boe Prox - 12/17/2015
+                    - Initial build
+
+        .EXAMPLE
+            #Review PowerShell process threads
+            (Get-Process PowerShell).Threads | Format-Table
+
+            #Pick a thread to remove
+            Stop-Thread -ThreadID 6376
+
+            Description
+            -----------
+            Terminates a thread on the PowerShell process
+
+        .EXAMPLE
+            #Open notepad process and get ThreadIds
+            Start-Process Notepad
+
+            #Stop the ThreadID
+            (Get-Process Notepad).Threads.Id | Stop-Thread -Verbose
+
+            VERBOSE: Performing the operation "Terminate thread" on target "4360".
+
+            Description
+            -----------
+            Stops notepad completely as it only has a single thread.
+    #>
+    [cmdletbinding(SupportsShouldProcess = $True)]
+    Param (
+        [parameter(ValueFromPipeline=$True)]
+        [intptr[]]$ThreadID
+    )
+    Begin {
+        Try {
+            [void][ThreadStop]
+        } Catch {
+            Write-Verbose "Building pinvoke via reflection"
+            #region Module Builder
+            $Domain = [AppDomain]::CurrentDomain
+            $DynAssembly = New-Object System.Reflection.AssemblyName('ThreadStop')
+            $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run) # Only run in memory
+            $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('ThreadStop', $False)
+            #endregion Module Builder
+
+            #region ENUMs
+            #region LSA_AccessPolicy
+            $EnumBuilder = $ModuleBuilder.DefineEnum('ThreadAccess', 'Public', [uint32])
+            [void]$EnumBuilder.DefineLiteral('TERMINATE', [uint32] 0x0001)
+            [void]$EnumBuilder.DefineLiteral('SUSPEND_RESUME', [uint32] 0x0002)
+            [void]$EnumBuilder.DefineLiteral('GET_CONTEXT', [uint32] 0x0008)
+            [void]$EnumBuilder.DefineLiteral('SET_CONTEXT', [uint32] 0x0010)
+            [void]$EnumBuilder.DefineLiteral('SET_INFORMATION', [uint32] 0x0020)
+            [void]$EnumBuilder.DefineLiteral('QUERY_INFORMATION', [uint32] 0x0040)
+            [void]$EnumBuilder.DefineLiteral('SET_THREAD_TOKEN', [uint32] 0x0080)
+            [void]$EnumBuilder.DefineLiteral('IMPERSONATE', [uint32] 0x0100)
+            [void]$EnumBuilder.DefineLiteral('DIRECT_IMPERSONATION', [uint32] 0x0200)
+            [void]$EnumBuilder.CreateType()
+            #endregion LSA_AccessPolicy
+            #endregion ENUMs
+
+            $TypeBuilder = $ModuleBuilder.DefineType('ThreadStop', 'Public, Class')
+
+            #region METHODS
+            #region OpenThread Method
+            $PInvokeMethod = $TypeBuilder.DefineMethod(
+                'OpenThread', #Method Name
+                [Reflection.MethodAttributes] 'PrivateScope, Public, Static, HideBySig, PinvokeImpl', #Method Attributes
+                [uint32], #Method Return Type
+                [Type[]] @(
+                    [int32],  #Desired Access
+                    [bool],   #Inherit Handle
+                    [int32]   #Thread ID
+                ) #Method Parameters
+            )
+
+            $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+            $FieldArray = [Reflection.FieldInfo[]] @(
+                [Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint'),
+                [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+                [Runtime.InteropServices.DllImportAttribute].GetField('PreserveSig')
+                [Runtime.InteropServices.DllImportAttribute].GetField('CharSet')
+            )
+
+            $FieldValueArray = [Object[]] @(
+                'OpenThread', #CASE SENSITIVE!!
+                $True,
+                $True,
+                [System.Runtime.InteropServices.CharSet]::Auto
+            )
+
+            $CustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder(
+                $DllImportConstructor,
+                @('kernel32.dll'),
+                $FieldArray,
+                $FieldValueArray    
+            )
+
+            $PInvokeMethod.SetCustomAttribute($CustomAttribute)
+            #endregion OpenThread Method
+            #region TerminateThread Method
+            $PInvokeMethod = $TypeBuilder.DefineMethod(
+                'TerminateThread', #Method Name
+                [Reflection.MethodAttributes] 'PrivateScope, Public, Static, HideBySig, PinvokeImpl', #Method Attributes
+                [uint32], #Method Return Type
+                [Type[]] @(
+                    [intptr], #Thread
+                    [int32]   #ExitCode
+
+                ) #Method Parameters
+            )
+
+            $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+            $FieldArray = [Reflection.FieldInfo[]] @(
+                [Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint'),
+                [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+                [Runtime.InteropServices.DllImportAttribute].GetField('ExactSpelling')
+                [Runtime.InteropServices.DllImportAttribute].GetField('PreserveSig')
+            )
+
+            $FieldValueArray = [Object[]] @(
+                'TerminateThread', #CASE SENSITIVE!!
+                $True,
+                $True,
+                $True
+            )
+
+            $CustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder(
+                $DllImportConstructor,
+                @('kernel32.dll'),
+                $FieldArray,
+                $FieldValueArray
+            )
+
+            $PInvokeMethod.SetCustomAttribute($CustomAttribute)
+            #endregion TerminateThread Method
+            #region CloseHandle Method
+            $PInvokeMethod = $TypeBuilder.DefineMethod(
+                'CloseHandle', #Method Name
+                [Reflection.MethodAttributes] 'PrivateScope, Public, Static, HideBySig, PinvokeImpl', #Method Attributes
+                [bool], #Method Return Type
+                [Type[]] @(
+                    [intptr] #Handle
+                ) #Method Parameters
+            )
+
+            $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+            $FieldArray = [Reflection.FieldInfo[]] @(
+                [Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint'),
+                [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+                [Runtime.InteropServices.DllImportAttribute].GetField('ExactSpelling')
+                [Runtime.InteropServices.DllImportAttribute].GetField('PreserveSig')
+            )
+
+            $FieldValueArray = [Object[]] @(
+                'CloseHandle', #CASE SENSITIVE!!
+                $True,
+                $True,
+                $True
+            )
+
+            $CustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder(
+                $DllImportConstructor,
+                @('kernel32.dll'),
+                $FieldArray,
+                $FieldValueArray
+            )
+
+            $PInvokeMethod.SetCustomAttribute($CustomAttribute)
+            #endregion CloseHandle Method
+            #endregion METHODS
+
+            [void]$TypeBuilder.CreateType()
+        }    
+        $CurrentThread = [appdomain]::GetCurrentThreadId()
+        $ExitCode = 1
+    }
+    Process {
+        ForEach ($Thread in $ThreadID) {
+            If ($Thread -eq $CurrentThread) {
+                Write-Warning "Skipping ThreadID: $($Thread) as it is the current thread."
+                Continue
+            } Else {
+                $Handle = [ThreadStop]::OpenThread([ThreadAccess]::TERMINATE, $False, $Thread)
+                If ($Handle -gt 0) {
+                    If ($PSCmdlet.ShouldProcess($Thread,'Terminate thread')) {
+                        Try {
+                            $Return = [ThreadStop]::TerminateThread($Handle, $ExitCode)
+                            If ($Return -eq 0) {
+                                Write-Warning "Unable to close Thread: $($Thread)!"
+                            }
+                        } Catch {
+                            Write-Warning $_
+                        }
+                    }
+                    [void][ThreadStop]::CloseHandle($Handle)
+                } Else {
+                    Write-Warning "Couldn't open or find Thread ID: $($Thread)!"
+                }
+            }
+        }
+    }
+}
--- edr.ps1
+++ edr.ps1
@@ -19,13 +19,15 @@
 	Ensure that Registry file is imported to allow WMI event Subscription to the windows event log
 	
 	Your sysmon Rulename must contain Alert with the following response tags:
-	kp=y 	Kill Processes
+	kp=y 	Kill process with child processes
 	kpp=y 	Kill Parent Processes & all Child Processes
 	kc=y    Kill network connections
+	ki=y    Kill Injected Thread
 	sd=y 	Shutdown System
 	fw=y	Add Windows Firewall Rule to block inbound/outbound network connectivity from process
 	yara=y  Yara Scan file
 	ydel=y  Delete on Yara Detection
+	rf=y    Restore Deleted File
 	nr=y 	Null route ip (Unfinished)
 	
 	You can add Multiple tags for multiple Live responses
@@ -51,7 +53,7 @@ $Force = "TRUE"
 $cports = "C:\programdata\edr\cports.exe"
 $yararules = "C:\programdata\edr\yararules\china_chopper.yar"
 $notification = "A Suspicious event was detected on your system, notify the SOC Team immediately!"
-
+ipmo C:\programdata\edr\stop-thread.ps1
 Register-WmiEvent -Query "Select * From __InstanceCreationEvent Where TargetInstance ISA 'Win32_NTLogEvent' AND TargetInstance.LogFile='Microsoft-Windows-Sysmon/Operational' AND TargetInstance.Message Like '%Alert%'" -SourceIdentifier "Sysmon"
 Try{
 	While ($True) {
@@ -281,6 +283,81 @@ Try{
             else {
             }
         }
+        #Remote Thread Events
+		if($EventID -eq 8)
+        {
+            $msg = $Message -split "`r`n"
+			$msg2 = $msg.replace(': ','=').replace('\','\\')
+			$msg3 = $msg2 | Select -Skip 2
+			$sysmon = $msg3 | ConvertFrom-StringData
+			#
+            #Debug $Message uncomment to remove
+            foreach($i in $msg){Write-Host $i}
+			#
+			# Key/Value Tags from Sysmon RuleName
+			$data = $msg[1] -split ','
+			$data2 = $data |ConvertFrom-StringData
+			$MitreRef = $data2."MitreRef"
+			$Technique = $data2."Technique"
+			$Tactic = $data2."Tactic"
+			$Alert = $data2."Alert"
+			#
+			# Sysmon Event ID 8 Vars
+			$UtcTime = $sysmon."UtcTime"
+			$SourceProcessGuid = $sysmon."SourceProcessGuid"
+			$SourceProcessId = $sysmon."SourceProcessId"
+			$SourceImage = $sysmon."SourceImage"
+			$TargetProcessGuid = $sysmon."TargetProcessGuid"
+			$TargetProcessId = $sysmon."TargetProcessId"
+			$TargetImage = $sysmon."TargetImage"
+			$NewThreadId = $sysmon."NewThreadId"
+			$StartModule = $sysmon."StartModule"
+			$StartFunction = $sysmon."StartFunction"
+			#
+			# Begin Actions
+            if($msg[1].ToLower().Contains("alert=")) #Mitre Attack Desktop Alerts
+            {
+				# Desktop Alerts
+                Write-Host "[+] Alert: $Alert Process: $SourceImage Created Remote Thread within $TargetImage with Function $StartFunction from Thread ID: $NewThreadId at $UtcTime"
+                if($Notify){
+                    $message = "Alert: " + $Alert + "`n" + "Technique: " + $Technique + "`n" + "Tactic: " + $Tactic + "`n" + " $Alert Process: $SourceImage Created Remote Thread within $TargetImage with Function $StartFunction from Thread ID: $NewThreadId at $UtcTime" + "`n" + "$notification"
+                    $message | msg *
+                }
+				#Process Killer
+				if($msg[1].ToLower().Contains("kp=y")){
+					Write-Host "[+] Killing: $ProcessId"
+					taskkill /F /T /PID $ProcessId
+				}
+				#Injected Thread Killer
+				if($msg[1].ToLower().Contains("ki=y")){
+					Write-Host "[+] Killing Remote Thread: $NewThreadId"
+					Stop-Thread -ThreadId $NewThreadId
+				}
+				# Yara Scan
+				if($msg[1].ToLower().Contains("yara=y")){
+					Write-Host "[+] Scanning $Image with Yara..."
+					$result = C:\programdata\edr\yara64.exe -c $yararules "$Image"
+					if($result -gt "0"){ 
+					Write-Host "[+] Yara detected file $Image as malicious or suspicious..."
+					if($msg[1].ToLower().Contains("ydel=y")){
+						Write-Host "[+] Deleting file $Image"
+						Remove-Item -Path "$Image" -Force
+					}
+                    }
+					else {
+						Write-Host "[-] Yara detected file as clean.."
+					}
+				}
+				# Firewall Blocking
+				if($msg[1].ToLower().Contains("fw=y")){
+					Write-Host "[+] Blocking process: $image from internet access..."
+					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image in" protocol=tcp dir=in enable=yes action=block profile=any program="$Image"
+					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image out" protocol=tcp dir=out enable=yes action=block profile=any program="$Image"
+				}
+			}
+            else {
+            }
+        }
         #File Create Event Detection and Response
 		if($EventID -eq 11)
         {
@@ -496,6 +573,86 @@ Try{
 					Write-Host "[+] Blocking process: $image from internet access..."
 					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image in" protocol=tcp dir=in enable=yes action=block profile=any program="$Image"
 					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image out" protocol=tcp dir=out enable=yes action=block profile=any program="$Image"
+				}
+			}
+            else {
+            }
+        }
+        #File Delete Events
+		if($EventID -eq 23)
+        {
+            $msg = $Message -split "`r`n"
+			$msg2 = $msg.replace(': ','=').replace('\','\\')
+			$msg3 = $msg2 | Select -Skip 2
+			$sysmon = $msg3 | ConvertFrom-StringData
+			#
+            #Debug $Message uncomment to remove
+            foreach($i in $msg){Write-Host $i}
+			#
+			# Key/Value Tags from Sysmon RuleName
+			$data = $msg[1] -split ','
+			$data2 = $data |ConvertFrom-StringData
+			$MitreRef = $data2."MitreRef"
+			$Technique = $data2."Technique"
+			$Tactic = $data2."Tactic"
+			$Alert = $data2."Alert"
+			#
+			# Sysmon Event ID 22 Vars
+			$UtcTime = $sysmon."UtcTime"
+			$ProcessGuid = $sysmon."ProcessGuid"
+			$ProcessId = $sysmon."ProcessId"
+			$User = $sysmon."User"
+			$Image = $sysmon."Image"
+			$TargetFilename = $sysmon."TargetFilename"
+			$Hashes = $sysmon."Hashes"
+			$IsExecutable = $sysmon."IsExecutable"
+			$Archived = $sysmon."Archived"
+			#
+			# Begin Actions
+            if($msg[1].ToLower().Contains("alert=")) #Mitre Attack Desktop Alerts
+            {
+				# Desktop Alerts
+                Write-Host "[+] Alert: $Alert Process: $Image Deleted File $TargetFilename from Process ID: $ProcessId at $UtcTime"
+                if($Notify){
+                    $message = "Alert: " + $Alert + "`n" + "Technique: " + $Technique + "`n" + "Tactic: " + $Tactic + "`n" + " $Alert Process: $Image Deleted File $TargetFilename from Process ID: $ProcessId at $UtcTime" + "`n" + "$notification"
+                    $message | msg *
+                }
+				#Process Killer
+				if($msg[1].ToLower().Contains("kp=y")){
+					Write-Host "[+] Killing: $ProcessId"
+					taskkill /F /T /PID $ProcessId
+				}
+				# Yara Scan
+				if($msg[1].ToLower().Contains("yara=y")){
+					Write-Host "[+] Scanning $Image with Yara..."
+					$result = C:\programdata\edr\yara64.exe -c $yararules "$Image"
+					if($result -gt "0"){ 
+					Write-Host "[+] Yara detected file $Image as malicious or suspicious..."
+					if($msg[1].ToLower().Contains("ydel=y")){
+						Write-Host "[+] Deleting file $Image"
+						Remove-Item -Path "$Image" -Force
+					}
+                    }
+					else {
+						Write-Host "[-] Yara detected file as clean.."
+					}
+				}
+				# Firewall Blocking
+				if($msg[1].ToLower().Contains("fw=y")){
+					Write-Host "[+] Blocking process: $image from internet access..."
+					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image in" protocol=tcp dir=in enable=yes action=block profile=any program="$Image"
+					netsh advfirewall firewall add rule name="Sysmon EDR Block $Image out" protocol=tcp dir=out enable=yes action=block profile=any program="$Image"
+				}
+				# File Restore
+				if($msg[1].ToLower().Contains("rf=y")){
+					$pattern = "(MD5=|\,SHA256=|\,IMPHASH=)"
+					$File = $Hashes -replace $pattern
+					$TargetFilename -match ".([A-Za-z0-9]+)$"
+					$ext = $matches[0]
+					$TargetFilename -match "^([a-z]):"
+					$drv = $matches[0]
+					Write-Host "[+] Restoring: $drv\DeletedFiles\$file$ext to Original Location: $TargetFilename..."
+					Move-Item -Path "$drv\DeletedFiles\$File$ext" -Destination "$TargetFilename" -Force
 				}
 			}
             else {
